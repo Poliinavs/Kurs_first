a72a53d1d164a66529d2e16363210a7699d6fc8b
Polina Avsukevich <polina.avs@gmail.com>
2022-09-17 23:27:55 +0300

    added

---
+++ exam_dif/Любимка/Рп/Рп/Рп.cpp
@@ -0,0 +1,14 @@
+﻿// Рп.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char a[] = { 'a', 'b'};
+	int b = sizeof(a);
+	cout << b;
+
+}
+
